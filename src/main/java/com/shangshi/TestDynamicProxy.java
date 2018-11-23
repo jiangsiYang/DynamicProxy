f60566155d300dfa83b05b4ba4af217bf3bc032c
yangjiangsi <yangjiangsi@qunjielong.com>
2018-11-23 17:14:19 +0800

    理解深了一点动态代理

--- src/main/java/com/shangshi/TestDynamicProxy.java
+++ src/main/java/com/shangshi/TestDynamicProxy.java
@@ -4,6 +4,7 @@ public class TestDynamicProxy {
     public static void main(String args[]) {
         ProxyHandler proxyHandler = new ProxyHandler();
         Subject subject = (Subject) proxyHandler.bind(new RealSubject());
+        //研究一下这个subject，发现这个对象的类型是com.sun.proxy.$Proxy0,父类是java.lang.reflect.Proxy，所有的JDK动态代理都会继承这个类；同时实现了interface com.shangshi.Subject接口，只是这里把它的超类转化为Subject类型
         String result =  subject.doSomething(1);
         System.out.println(result);
 
