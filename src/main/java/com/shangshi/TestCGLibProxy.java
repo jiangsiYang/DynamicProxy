f8c9f36e25a7ca412216830839af70c05a2ef5a6
yangjiangsi <yangjiangsi@qunjielong.com>
2018-11-23 17:55:23 +0800

    CGLIB的一点理解

--- src/main/java/com/shangshi/TestCGLibProxy.java
+++ src/main/java/com/shangshi/TestCGLibProxy.java
@@ -6,6 +6,7 @@ package com.shangshi;
 public class TestCGLibProxy {
     public static void main(String args[]) {
         Subject subject = CGLibProxy.getInstance().getProxy(RealSubject.class);
+        //研究一下这个subject，发现这个对象的类型是com.shangshi.RealSubject$$EnhancerByCGLIB$$1f2b4198,这是CGLIB动态生成的类型,父类是com.shangshi.RealSubject，印证了CGLIB是通过继承实现代理；同时实现了net.sf.cglib.proxy.Factory接口，这个接口是CGLIB自己加入的，包含一些工具方法。
         String result = subject.doSomething(1);
         System.out.println(result);
 
